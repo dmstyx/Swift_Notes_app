dcb49d7a7f7e199e8de91a16d8474dc3d309af78
TK <synova_matare@hotmail.co.uk>
2022-02-04 10:35:32 +0000

    Initial Commit

---
+++ Shared/NotesMacOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesMacOsApp.swift
+//  Shared
+//
+//  Created by Carlos on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesMacOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
